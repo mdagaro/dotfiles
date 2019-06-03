fa3c5f1b9cabfe8a83f9618e72c250a667e183c5
Mateo D'Agaro <mateod@concordic.sea.i.extrahop.com>
2019-06-03 11:48:26 -0700

    Renamed files

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 # Install bashrc
 cd ~/dotfiles
-cat .bashrc > ~/.bashrc
+cat bashrc > ~/.bashrc
 cd
 touch .bashrc_local
 echo "\" Local bashrc file. Edit as you please!" > .bashrc_local
 
 cd ~/dotfiles
-cat .gitconfig > ~/.gitconfig
+cat gitconfig > ~/.gitconfig
 
 git clone --depth=1 https://github.com/mdagaro/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
