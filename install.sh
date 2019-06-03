1e341c11c38fbe548836cf750ae00da07fc44c2c
Mateo D'Agaro <mateod@extrahop.com>
2019-06-03 11:17:25 -0700

    Added gitconfig

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
+# Install bashrc
 cd ~/dotfiles
-cat ~/.bashrc > ~/.bashrc
+cat .bashrc > ~/.bashrc
 cd
 touch .bashrc_local
 echo "\" Local bashrc file. Edit as you please!" > .bashrc_local
+
+cd ~/dotfiles
+cat .gitconfig > ~/.gitconfig
 
 echo "Dotfiles installed"
