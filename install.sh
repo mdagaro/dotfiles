1800a6c1cd48ede8e1a0dd1f34516176fbdab5c0
Mateo D'Agaro <mateod@concordic.sea.i.extrahop.com>
2019-06-03 11:46:51 -0700

    Added vim installation to dotfiles

--- install.sh
+++ install.sh
@@ -8,4 +8,7 @@ echo "\" Local bashrc file. Edit as you please!" > .bashrc_local
 cd ~/dotfiles
 cat .gitconfig > ~/.gitconfig
 
+git clone --depth=1 https://github.com/mdagaro/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
 echo "Dotfiles installed"
