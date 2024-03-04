b75a61bdc744a4b41f2b86c0af15ef55912fa3b2
Mateo D'Agaro <mateod@extrahop.com>
2024-03-04 11:29:34 -0800

    Softlink neovim config during install

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 # Install bashrc
-cd ~/dotfiles
+
+DIRNAME=$(dirname $0)
+
+cd $DIRNAME
 cat bashrc > ~/.bashrc
 cat tmux > ~/.tmux.conf
-cd
-if [ ! -f .bashrc_local ]; then
-    touch .bashrc_local
+if [ ! -f ~/.bashrc_local ]; then
+    touch ~/.bashrc_local
     echo "# Local bashrc file. Edit as you please!" > .bashrc_local
 fi
 
-cd ~/dotfiles
 cat gitconfig > ~/.gitconfig
 
 git submodule update --init --recursive
 
-bash ./vim/install_awesome_vimrc.sh
+ln -s $DIRNAME/neovim ~/.config/nvim
 
 echo "Dotfiles installed"
 
